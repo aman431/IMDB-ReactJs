40db53de614950609ac75d6c7e08d034bd294dd2
Aman Rathore <amanrathod777@gmail.com>
2020-10-06 20:10:26 +0530

    Update

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,11 @@ function Home() {
         <div>
             <div className="top">
                 <p>Amazon Original</p>
-                <img src="https://images-na.ssl-images-amazon.com/images/G/02/shazam/JKRY-S2-01598-DigitalROSStatic-ARSDE-970x250-Hero-ADSP-02-POST-e3968c9e-ac56-48a1-a705-2280146f1e05.jpg" alt=""/>
+                <img src="https://images-na.ssl-images-amazon.com/images/G/02/shazam/MOLV-S1-01137-PMHouse-970x250-POST-Final-en-US-HO-PVD2765-KvObS-a9a98a6b-3fbe-4e11-952b-facb7a425310.jpg" alt=""/>
             </div>
             <div className="Carousel">
-                 <Carousel >
-                    <img src="https://www.imdb.com/list/ls053181649/videoplayer/vi1887486233?ref_=hm_hp_i_8" alt=""/>
+                 <Carousel className="arrow">
+                    <img src="https://images-na.ssl-images-amazon.com/images/G/02/shazam/JKRY-S2-01598-DigitalROSStatic-ARSDE-970x250-Hero-ADSP-02-POST-e3968c9e-ac56-48a1-a705-2280146f1e05.jpg" alt=""/>
                     <img src="https://images-na.ssl-images-amazon.com/images/G/02/shazam/JKRY-S2-01598-DigitalROSStatic-ARSDE-970x250-Hero-ADSP-02-POST-e3968c9e-ac56-48a1-a705-2280146f1e05.jpg" alt=""/>
                     <img src="https://images-na.ssl-images-amazon.com/images/G/02/shazam/JKRY-S2-01598-DigitalROSStatic-ARSDE-970x250-Hero-ADSP-02-POST-e3968c9e-ac56-48a1-a705-2280146f1e05.jpg" alt=""/>
                     <img src="https://images-na.ssl-images-amazon.com/images/G/02/shazam/JKRY-S2-01598-DigitalROSStatic-ARSDE-970x250-Hero-ADSP-02-POST-e3968c9e-ac56-48a1-a705-2280146f1e05.jpg" alt=""/>
